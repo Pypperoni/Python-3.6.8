ef8bcc631ff403bc4e230ed9b62234497967febf
loblao <12ksit@gmail.com>
2019-05-28 16:39:02 -0300

    collections: Provide a custom namedtuple

--- Lib/collections/__init__.py
+++ Lib/collections/__init__.py
@@ -302,61 +302,143 @@ except ImportError:
 ### namedtuple
 ################################################################################
 
-_class_template = """\
 from builtins import property as _property, tuple as _tuple
 from operator import itemgetter as _itemgetter
-from collections import OrderedDict
-
-class {typename}(tuple):
-    '{typename}({arg_list})'
-
-    __slots__ = ()
-
-    _fields = {field_names!r}
-
-    def __new__(_cls, {arg_list}):
-        'Create new instance of {typename}({arg_list})'
-        return _tuple.__new__(_cls, ({arg_list}))
-
-    @classmethod
-    def _make(cls, iterable, new=tuple.__new__, len=len):
-        'Make a new {typename} object from a sequence or iterable'
-        result = new(cls, iterable)
-        if len(result) != {num_fields:d}:
-            raise TypeError('Expected {num_fields:d} arguments, got %d' % len(result))
-        return result
-
-    def _replace(_self, **kwds):
-        'Return a new {typename} object replacing specified fields with new values'
-        result = _self._make(map(kwds.pop, {field_names!r}, _self))
-        if kwds:
-            raise ValueError('Got unexpected field names: %r' % list(kwds))
-        return result
-
-    def __repr__(self):
-        'Return a nicely formatted representation string'
-        return self.__class__.__name__ + '({repr_fmt})' % self
-
-    def _asdict(self):
-        'Return a new OrderedDict which maps field names to their values.'
-        return OrderedDict(zip(self._fields, self))
-
-    def __getnewargs__(self):
-        'Return self as a plain tuple.  Used by copy and pickle.'
-        return tuple(self)
-
-{field_defs}
-"""
-
-_repr_template = '{name}=%r'
-
-_field_template = '''\
-    {name} = _property(_itemgetter({index:d}), doc='Alias for field number {index:d}')
-'''
 
 # Pypperoni HACK
-namedtuple = lambda *x: tuple
+def namedtuple(typename, field_names, *, verbose=False, rename=False, module=None):
+    """Returns a new subclass of tuple with named fields.
+    >>> Point = namedtuple('Point', ['x', 'y'])
+    >>> Point.__doc__                   # docstring for the new class
+    'Point(x, y)'
+    >>> p = Point(11, y=22)             # instantiate with positional args or keywords
+    >>> p[0] + p[1]                     # indexable like a plain tuple
+    33
+    >>> x, y = p                        # unpack like a regular tuple
+    >>> x, y
+    (11, 22)
+    >>> p.x + p.y                       # fields also accessible by name
+    33
+    >>> d = p._asdict()                 # convert to a dictionary
+    >>> d['x']
+    11
+    >>> Point(**d)                      # convert from a dictionary
+    Point(x=11, y=22)
+    >>> p._replace(x=100)               # _replace() is like str.replace() but targets named fields
+    Point(x=100, y=22)
+    """
 
+    # Validate the field names.  At the user's option, either generate an error
+    # message or automatically replace the field name with a valid name.
+    if isinstance(field_names, str):
+        field_names = field_names.replace(',', ' ').split()
+    field_names = list(map(str, field_names))
+    typename = str(typename)
+    if rename:
+        seen = set()
+        for index, name in enumerate(field_names):
+            if (not name.isidentifier()
+                or _iskeyword(name)
+                or name.startswith('_')
+                or name in seen):
+                field_names[index] = '_%d' % index
+            seen.add(name)
+    for name in [typename] + field_names:
+        if type(name) is not str:
+            raise TypeError('Type names and field names must be strings')
+        if not name.isidentifier():
+            raise ValueError('Type names and field names must be valid '
+                             'identifiers: %r' % name)
+        if _iskeyword(name):
+            raise ValueError('Type names and field names cannot be a '
+                             'keyword: %r' % name)
+    seen = set()
+    for name in field_names:
+        if name.startswith('_') and not rename:
+            raise ValueError('Field names cannot start with an underscore: '
+                             '%r' % name)
+        if name in seen:
+            raise ValueError('Encountered duplicate field name: %r' % name)
+        seen.add(name)
+
+    field_names = tuple(field_names)
+    num_fields = len(field_names)
+
+    _repr_template = '{name}=%r'
+    repr_fmt = ', '.join(_repr_template.format(name=name) for name in field_names)
+
+    _UNSET = object()
+    class _KlassDef(tuple):
+        __slots__ = ()
+        _fields = field_names
+
+        def __new__(_cls, *args, **kwargs):
+            if len(args) > len(field_names):
+                msg = '__new__() takes %d positional arguments but %d were given'
+                msg = msg % (len(field_names) + 1, len(args) + 1)
+                raise TypeError(msg)
+
+            values = list(args) + [_UNSET] * (len(field_names) - len(args))
+            for k, v in kwargs.items():
+                try:
+                    i = field_names.index(k)
+
+                except ValueError:
+                    msg = "__new__() got an unexpected keyword argument '%s'"
+                    msg = msg % k
+                    raise TypeError(msg)
+
+                if values[i] is not _UNSET:
+                    msg = "__new__() got multiple values for argument '%s'"
+                    msg = msg % k
+                    raise TypeError(msg)
+
+                values[i] = v
+
+            missing = []
+            for i, v in enumerate(values):
+                if v is _UNSET:
+                    missing.append(field_names[i])
+
+            if missing:
+                msg = "__new__() missing %d required positional argument%s:"
+                msg = msg % (len(missing), 's' if len(missing) > 1 else '')
+                msg += ', '.join("'%s'" % m for m in missing)
+                if ',' in msg:
+                    msg = msg.rsplit(',', 1)[0] + " and '%s'" % missing[-1]
+
+                raise TypeError(msg)
+
+            return _tuple.__new__(_cls, values)
+
+        @classmethod
+        def _make(cls, iterable, new=tuple.__new__):
+            result = new(cls, iterable)
+            if len(result) != num_fields:
+                raise TypeError('Expected %d arguments, got %d' % (num_fields, len(result)))
+            return result
+
+        def _replace(_self, **kwds):
+            result = _self._make(map(kwds.pop, field_names, _self))
+            if kwds:
+                raise ValueError('Got unexpected field names: %r' % list(kwds))
+            return result
+
+        def __repr__(self):
+            return self.__class__.__name__ + f'({repr_fmt})' % self
+
+        def _asdict(self):
+            return OrderedDict(zip(self._fields, self))
+
+        def __getnewargs__(self):
+            return tuple(self)
+
+    for index, name in enumerate(field_names):
+        field = _property(_itemgetter(index), doc=f'Alias for field number {index:d}')
+        setattr(_KlassDef, name, field)
+
+    _KlassDef.__name__ = typename
+    return _KlassDef
 
 ########################################################################
 ###  Counter
